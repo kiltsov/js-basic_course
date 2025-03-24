425296eb11e319fbb158bfc7714d57b29d12d2cb
Kiltsov Anton <kiltsov.anton@gmail.com>
2025-03-24 13:21:27 +0300

    DOM

--- DOM/scripts/app.js
+++ DOM/scripts/app.js
@@ -56,6 +56,12 @@ function submitForm () {
     document.querySelector('.notification').setAttribute('key', 1);
     document.querySelector('.notification').setAttribute('user-id', 1);
     console.log(Number(document.querySelector('.notification').getAttribute('user-id', 1)));
+
+    //
+    // 17.11. Упражнение - сохранение JSON
+    //
+    const localStorTest = JSON.stringify({text: input});
+    localStorage.setItem('text', localStorTest);
 };
 
 // По нажатию на Enter
@@ -72,3 +78,53 @@ function inputChanged(e) {
         submitForm();
       }
 };
+
+
+//
+// 17.7. Упражнение - получение DOM элементов
+//
+const elements = document.querySelectorAll('.one span');
+console.log(elements[0].innerText);
+console.log(elements[1].innerText);
+console.log(document.querySelector('#two').innerText);
+console.log(document.querySelector('[user-id="4"]').innerText);
+
+console.log('----------');
+
+console.log(document.querySelectorAll('.one')[0].innerText);
+console.log(document.querySelectorAll('.one')[1].innerText);
+console.log(document.querySelector('.one ~ div').innerText); //  Хак, не очень хорошо
+console.log(document.getElementById('two').innerText); // ID
+
+//
+// 17.8. Добавление html на лету
+//
+const panelText = 'Панель';
+const panelClass = 'button';
+const newElement = document.createElement('div');
+newElement.setAttribute('user-id', 1);
+newElement.classList.add('panel');
+// newElement.innerText = 'Кнопка';
+newElement.innerHTML = `<button class="${panelClass}">${panelText}</button>`;
+document.querySelector('.test').appendChild(newElement);
+
+
+//
+// 17.9. Local storage
+//
+
+// Смотреть - Приложение => Хранилище => Локальное хранилище
+localStorage.setItem('token', 'sdfhjk2sdfkj'); // Добавить - ключ - значение
+localStorage.setItem('token1', 1);
+localStorage.setItem('token1', true); // Все будет преобразовано в строку
+localStorage.removeItem('token1'); // Удалить по ключу
+localStorage.clear(); // Очистить весь сторадж
+
+//
+// 17.10. JSON
+//
+
+const obj = JSON.parse('{ "a": 1 }'); // Преобразует в обьект
+console.log(obj);
+const str = JSON.stringify(obj); // Преобразует в строку
+console.log(str);
