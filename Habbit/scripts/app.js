8006744c0cb11710bdcaec4f154f57cdfffd8606
Kiltsov Anton <kiltsov.anton@gmail.com>
2025-03-28 19:18:24 +0300

    Habbit done

--- Habbit/scripts/app.js
+++ Habbit/scripts/app.js
@@ -208,6 +208,7 @@ function rerender(activeHabbitId) {
   if (!activeHabbit) {
     return;
   }
+  document.location.replace(document.location.pathname + '#' + activeHabbitId);
   rerenderMenu(activeHabbit);
   rerenderHead(activeHabbit);
   rerenderContent(activeHabbit);
@@ -216,5 +217,11 @@ function rerender(activeHabbitId) {
 // Запуск приложения
 (() => {
   loadData();
-  rerender(habbits[0].id);
+  const hashId = Number(document.location.hash.replace('#', ''));
+  const urlHabbit = habbits.find(habbit => habbit.id == hashId);
+  if (urlHabbit) {
+    rerender(urlHabbit.id);
+  } else {
+    rerender(habbits[0].id);
+  }
 })();
